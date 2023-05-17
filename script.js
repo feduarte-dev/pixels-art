c63f04b533c41dee3ab4769dd5875d948340061a
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-17 15:36:10 -0300

    4 - Adicione um botão para gerar cores aleatórias para a paleta de cores

--- script.js
+++ script.js
@@ -13,4 +13,24 @@ for (let index = 0; index < 4; index += 1) {
   colorPalette.appendChild(color);
 }
 
-console.log(colorPalette);
+// Requisito 4.1
+const button = document.createElement('button');
+button.setAttribute('id', 'button-random-color');
+button.innerText = 'Cores aleatórias';
+body.appendChild(button);
+
+// Requisito 4.3
+const btn = document.querySelector('#button-random-color');
+const colors = document.querySelectorAll('.color');
+
+btn.addEventListener('click', () => {
+  for (let index = 1; index < colors.length; index += 1) {
+    const randomRed = Math.floor(Math.random() * 256);
+    const randomGreen = Math.floor(Math.random() * 256);
+    const randomBlue = Math.floor(Math.random() * 256);
+    const randomRGB = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+    colors[index].style.backgroundColor = randomRGB;
+  }
+});
+
+console.log(body);
